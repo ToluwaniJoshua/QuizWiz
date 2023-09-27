46e418659899a2fdbf9a1e1524d0fda4877d2640
ToluwaniJoshua <102393761+ToluwaniJoshua@users.noreply.github.com>
2023-09-27 02:29:14 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 // JavaScript for toggling the mobile menu
-
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+// Close the mobile menu when a menu item is clicked
+const menuItems = document.querySelectorAll(".menu a");
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
+
